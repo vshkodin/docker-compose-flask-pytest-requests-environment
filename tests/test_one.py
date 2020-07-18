82bb164c71fcae1bbef5b2dbf967403f9708fa6b
vlad <vladimir.shkodin@slickdeals.net>
2020-07-17 18:00:03 -0700

    added test

--- tests/test_one.py
+++ tests/test_one.py
@@ -5,8 +5,7 @@ def test_get(request):
     print(url)
     r =requests.get(url)
     assert 200 == r.status_code
-"""def test_post(request):
-
+def test_post(request):
     r = requests.post(f'{request.config.getoption("--base_url")}/', json={"name":"Kumar"})
     assert 200 == r.status_code
-    assert "Kumar" == r.json()['name']"""
+    assert "Kumar" == r.json()['name']
